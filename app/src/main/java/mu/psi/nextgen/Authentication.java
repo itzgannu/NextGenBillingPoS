e29e69ca68cb0ec5543d348c243b7206c68da208
Lakshmi Phani Ganesh <surepalli.ganesh@gmail.com>
2022-01-01 01:43:12 -0500

    Registration Added : 1, Registration.java, activity_registration.xml 2, progress_bar.xml 3, style.xml 4, border_corner.xml Modified : 1, activity_authentication.xml 2, activity_splash_screen.xml 3, AndroidManifest.xml 4, Authentication.java 5, colors.xml, dimens.xml, gradle.xml, strings.xml, misc.xml 6, SplashScreen.java

--- app/src/main/java/mu/psi/nextgen/Authentication.java
+++ app/src/main/java/mu/psi/nextgen/Authentication.java
@@ -1,21 +1,141 @@
 package mu.psi.nextgen;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import java.util.Objects;
 
 import mu.psi.nextgen.databinding.ActivityAuthenticationBinding;
 
-public class Authentication extends AppCompatActivity {
+public class Authentication extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
 
     ActivityAuthenticationBinding binding;
 
+    Dialog dialog;
+    AlertDialog.Builder builder;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         this.binding = ActivityAuthenticationBinding.inflate(getLayoutInflater());
         setContentView(this.binding.getRoot());
 
-        this.binding.authenticationScreenLogo.animate().alpha(1).setDuration(1800);
+        builder = new AlertDialog.Builder(this);
+        builder.setView(R.layout.progress_bar);
+        dialog = builder.create();
+
+        this.binding.authenticationScreenLogo.animate().alpha(1).setDuration(1000);
+
+        this.binding.authenticationScreenPasscode.setError(null);
+        this.binding.authenticationScreenEmailId.setError(null);
+
+        this.binding.authenticationScreenSignUpButton.setOnClickListener(this);
+        this.binding.authenticationScreenSignInButton.setOnClickListener(this);
+        this.binding.authenticationScreenForgotPasswordButton.setOnClickListener(this);
+        this.binding.authenticationScreen.setOnTouchListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        this.binding.authenticationScreenPasscode.setError(null);
+        this.binding.authenticationScreenEmailId.setError(null);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        this.binding.authenticationScreenPasscode.setError(null);
+        this.binding.authenticationScreenEmailId.setError(null);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if (id == R.id.authentication_screen_sign_up_button) {
+            onClickSignUp();
+        } else if (id == R.id.authentication_screen_sign_in_button) {
+            onClickSignIn();
+        } else if (id == R.id.authentication_screen_forgot_password_button) {
+            onClickForgotPassword();
+        }
+    }
+
+    void progress_bar(boolean show) {
+        if (show) {
+            dialog.show();
+        } else {
+            dialog.dismiss();
+        }
+    }
+
+    void onClickSignUp() {
+        Intent splashIntent = new Intent(Authentication.this, Registration.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(splashIntent);
+        finish();
+    }
+
+    void onClickSignIn() {
+        this.binding.authenticationScreenEmailId.clearFocus();
+        this.binding.authenticationScreenPasscode.clearFocus();
+        if (authenticationFieldValidations()) {
+            Intent splashIntent = new Intent(Authentication.this, Authentication.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(splashIntent);
+            finish();
+        }
+    }
+
+    void onClickForgotPassword() {
+        //firebase auth forgot password
+        //show dialog after sending email
+    }
+
+    boolean authenticationFieldValidations() {
+        String email = Objects.requireNonNull(this.binding.authenticationScreenEmailId.getEditText()).getText().toString();
+        String code = Objects.requireNonNull(this.binding.authenticationScreenPasscode.getEditText()).getText().toString();
+        String empty_field = getString(R.string.error_field_empty);
+        if (email.isEmpty() && code.isEmpty()) {
+            this.binding.authenticationScreenEmailId.setError(empty_field);
+            this.binding.authenticationScreenPasscode.setError(empty_field);
+            return false;
+        } else if (email.isEmpty()) {
+            this.binding.authenticationScreenEmailId.setError(empty_field);
+            this.binding.authenticationScreenPasscode.setError(null);
+            return false;
+        } else if (code.isEmpty()) {
+            this.binding.authenticationScreenPasscode.setError(empty_field);
+            this.binding.authenticationScreenEmailId.setError(null);
+            return false;
+        } else {
+            this.binding.authenticationScreenPasscode.setError(null);
+            this.binding.authenticationScreenEmailId.setError(null);
+            return true;
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (v.getId() == R.id.authentication_screen) {
+            InputMethodManager hideSoftKeyBoard = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            hideSoftKeyBoard.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            this.binding.authenticationScreenEmailId.clearFocus();
+            this.binding.authenticationScreenPasscode.clearFocus();
+            return true;
+        }
+        return false;
     }
 }
